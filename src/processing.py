2f1a99298009c1a24e3d81cb080f9b3a38ef24e8
Polina <polinasatrajkina@gmail.com>
2024-04-12 19:11:43 +0300

    добавила в модифайд полностью папку src

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+
+
 def get_new_list(list_of_dicts, state="EXECUTED"):
     new_list = []
     for i in list_of_dicts:
         if i["state"] == state:
             new_list.append(i)
-    print(new_list)
+    return new_list
 
 
-
+def get_sorted_list(list_of_dicts, order=False):
+    list_of_dicts.sort(key=lambda a: datetime.strptime(a['date'], "%Y-%m-%dT%H:%M:%S.%f"), reverse=order)
+    return list_of_dicts
