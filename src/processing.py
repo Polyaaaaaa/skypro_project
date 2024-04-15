223f9824a2abfad03e192126bbade76688a29785
Polina <polinasatrajkina@gmail.com>
2024-04-15 13:04:52 +0300

    add docstrings

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
 
-def get_new_list(list_of_dicts, state="EXECUTED"):
+def get_new_list(list_of_dicts: list, state: str = "EXECUTED") -> list:
+    """функция, фильтрующая список"""
     new_list = []
     for i in list_of_dicts:
         if i["state"] == state:
@@ -9,6 +10,7 @@ def get_new_list(list_of_dicts, state="EXECUTED"):
     return new_list
 
 
-def get_sorted_list(list_of_dicts, order=False):
-    list_of_dicts.sort(key=lambda a: datetime.strptime(a['date'], "%Y-%m-%dT%H:%M:%S.%f"), reverse=order)
+def get_sorted_list(list_of_dicts: list, order: bool = False) -> list:
+    """функция, сортирующая список"""
+    list_of_dicts.sort(key=lambda a: datetime.strptime(a["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=order)
     return list_of_dicts
