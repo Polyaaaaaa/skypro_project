a5974e7202c2b9e31106cd0b0c7ddb7649158e8f
Polina <polinasatrajkina@gmail.com>
2024-04-22 19:40:08 +0300

    add last generators

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,10 +5,12 @@ from src.processing import get_new_list, get_sorted_list
 
 @pytest.fixture
 def coll():  # имя фикстуры любое
-    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
 def test_get_new_list(coll) -> None:
@@ -17,15 +19,16 @@ def test_get_new_list(coll) -> None:
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 
-    assert get_new_list(coll, 'CANCELED') == [
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+    assert get_new_list(coll, "CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
 def test_to_sorted_list(coll) -> None:
     assert get_sorted_list(coll, order=True) == [
-               {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-               {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-               {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-               {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-           ]
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
