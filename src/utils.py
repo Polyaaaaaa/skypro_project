b1dfa46b9f23f0b2ef1c16e2929b4e20a7052332
Polina <polinasatrajkina@gmail.com>
2024-05-13 15:22:05 +0300

    add new tests for utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,60 +1,56 @@
 import json
+from typing import Any
 import requests
 
 API_KEY = "Y0NJSYiThQhh4r2ykGqyoeHJ8OISMbYU"
 
 
-def get_list_of_transactions(filepath: str) -> list:
+def get_list_of_transactions(filepath: str) -> Any:
     try:
-        with open(filepath, "r", encoding='utf-8') as file:
+        with open(filepath, "r", encoding="utf-8") as file:
             dicts_in_list = json.load(file)
     except json.decoder.JSONDecodeError:
+        return []
+    except FileNotFoundError:
         return []
     else:
         if len(dicts_in_list) > 0:
             return dicts_in_list
-        elif dicts_in_list is not list:
+        elif not isinstance(dicts_in_list, list):
             return []
 
-
-func = get_list_of_transactions("C:/Users/Student Free/PycharmProjects/pythonProject2/data/operations.json")
-print(func)
+    return []
 
 
-def get_sum_transactions(transaction):
+def get_sum_transactions(transaction: dict) -> float:
     summ = 0
-    amnt = transaction.get('operationAmount')['amount']
-    currency = transaction['operationAmount']['currency']['code']
+    amnt = transaction["operationAmount"]["amount"]
+    currency = transaction["operationAmount"]["currency"]["code"]
 
-    if currency != 'RUB':
+    if currency != "RUB":
 
         url = f"https://api.apilayer.com/exchangerates_data/convert?to={'RUB'}&from={currency}&amount={amnt}"
 
-        payload = {}
-        headers = {
-            "apikey": API_KEY
-        }
-
+        headers = {"apikey": API_KEY}
+        payload: dict = {}
         response = requests.request("GET", url, headers=headers, data=payload)
-        result = response.json().get('result')
+        result = response.json().get("result")
         summ += result
     else:
         summ += amnt
     return summ
 
 
-print(get_sum_transactions({
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-        "amount": "3.58",
-        "currency": {
-            "name": "руб.",
-            "code": "EUR"
+print(
+    get_sum_transactions(
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "3.58", "currency": {"name": "руб.", "code": "EUR"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
         }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-}))
+    )
+)
