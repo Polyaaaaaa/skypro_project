6ca7849acd0e87a1355d8d6c3135e9fb6582eec4
Polina <polinasatrajkina@gmail.com>
2024-05-17 12:14:06 +0300

    rewrite function type

--- src/utils.py
+++ src/utils.py
@@ -45,5 +45,17 @@ def get_sum_transactions(transaction: dict) -> float:
         result = response.json().get("result")
         summ += result
     else:
-        summ += amnt
+        summ += float(amnt)
     return summ
+
+
+print(get_sum_transactions({
+        "id": 441945886,
+        "state": "EXECUTED",
+        "date": "2019-08-26T10:50:58.294041",
+        "operationAmount": {
+            "amount": "31957.58",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }}}))
